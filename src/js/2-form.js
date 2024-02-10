ae8776399fd6c243e4da0e051f7300cd90aa12bb
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-02-10 18:56:22 +0900

    update form.css and 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,22 +1,27 @@
 const form = document.querySelector('.feedback-form');
 
-const localStorageKey = 'feedback-form';
+const localStorageKey = 'feedback-form-state';
 
 form.elements.email.value = localStorage.getItem(localStorageKey) || '';
 form.elements.message.value = localStorage.getItem(localStorageKey) || '';
 
 form.addEventListener('input', event => {
-  localStorage.setItem(localStorageKey, event.target.value);
+  const formInputValue = event.target.value.trim();
+  localStorage.setItem(localStorageKey, formInputValue);
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const inputValue = event.target.elements.email.value;
-  const messageValue = event.target.elements.message.value;
+  const inputValue = event.target.elements.email.value.trim();
+  const messageValue = event.target.elements.message.value.trim();
 
   const FormData = { email: inputValue, message: messageValue };
-  console.log(FormData);
+  if (inputValue !== '' && messageValue !== '') {
+    console.log(FormData);
 
-  localStorage.removeItem(localStorageKey);
-  form.reset();
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+  } else {
+    console.log('Please fill all feilds');
+  }
 });
